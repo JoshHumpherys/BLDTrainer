aca734697b0f5bbcf2df59f07ae8ce57b4774ba6
Josh Humpherys <joshua.humpherys@gmail.com>
2015-12-23 13:24:04 -0800

    Read images.txt

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 public class Main extends JFrame {
+	public static final String IMAGES_FILE_DIR = "images.txt";
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
@@ -19,6 +27,27 @@ public class Main extends JFrame {
 		setPreferredSize(new Dimension((int)(screenDimension.getWidth() * 2 / 3), (int)(screenDimension.getHeight() * 2 / 3)));
 		pack();
 		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		Map<String, String> pairs = new HashMap<String, String>();
+		try {
+			FileReader fr = new FileReader(IMAGES_FILE_DIR);
+			BufferedReader br = new BufferedReader(fr);
+			String line = null;
+			while((line = br.readLine()) != null) {
+				int i = line.indexOf(" ");
+				pairs.put(line.substring(0, i), line.substring(i + 1));
+			}
+		}
+		catch(FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, "Cannot find file: " + IMAGES_FILE_DIR);
+			System.exit(1);
+		}
+		catch(IOException e) {
+			JOptionPane.showMessageDialog(this, "Error reading file: " + IMAGES_FILE_DIR);
+			System.exit(1);
+		}
+		System.out.println(pairs);
 	}
 }
